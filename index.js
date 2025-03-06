7d9a44585ab8db5a978495260fa373aa3b244b4b
Piyush <piyushchoudhary.068@gmail.com>
2025-03-06 08:35:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const fs=require('fs');
-const text=fs.readFileSync('a.txt','utf8');  //synchronus
-console.log(text);
+const express=require("express")
+var cors = require('cors')
 
-const text2=fs.readFile('a.txt','utf8');  //assynchronus
-console.log(text2);
+const app=express();
+app.use(cors());
+app.use(express.json())
+
+app.post("/sum",(req,res)=>{
+    const a =parseInt(req.body.a);
+    const b=parseInt(req.body.b);
+    res.send({
+
+    sum:a + b
+    })
+})
+app.listen(3000);
